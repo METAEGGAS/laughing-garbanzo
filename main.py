763cac5754aa5d813604936646a47c3358dd29be
METAEGGAS <05318352256bna@gmail.com>
2026-07-12 15:32:39 +0300

    Update Firebase credentials file name

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 
 # اتصال Firebase
 cred = credentials.Certificate(
-    "hhhxh-5ebe4-firebase-adminsdk-fbsvc-45402e4ef4.json"
+    "serviceAccountKey.json"
 )
 
 firebase_admin.initialize_app(cred)
