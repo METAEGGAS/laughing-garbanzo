9f9030b8956da86382da6197215ca65a50e39268
METAEGGAS <05318352256bna@gmail.com>
2026-07-12 15:08:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials, firestore
+import asyncio
+import random
+import time
+
+
+# اتصال Firebase
+cred = credentials.Certificate(
+    "hhhxh-5ebe4-firebase-adminsdk-fbsvc-f3ecdb67a0.json"
+)
+
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+app = FastAPI()
+
+
+price = 100.00000
+
+
+async def price_generator():
+
+    global price
+
+    while True:
+
+        old_price = price
+
+        price += random.uniform(-0.05, 0.05)
+
+        candle = {
+            "time": int(time.time()),
+            "open": round(old_price, 5),
+            "high": round(max(old_price, price), 5),
+            "low": round(min(old_price, price), 5),
+            "close": round(price, 5),
+            "active": True
+        }
+
+
+        # حفظ السعر في Firestore
+        db.collection("candles") \
+          .document("EURUSD") \
+          .collection("1m") \
+          .document(str(candle["time"])) \
+          .set(candle)
+
+
+        print(candle)
+
+        await asyncio.sleep(1)
+
+
+
+@app.on_event("startup")
+async def start():
+
+    asyncio.create_task(price_generator())
